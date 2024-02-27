376838abf0e8b194d88b2a5a3375cbe7c813881e
Bogdan Oprisiu <bobo.oprisiu@gmail.com>
2024-02-27 22:32:39 +0200

    Ubung2

--- Ubung2/Labyrinth.py
+++ Ubung2/Labyrinth.py
@@ -1,6 +1,7 @@
-import heapq
-
+import matplotlib.pyplot as plt
 import numpy as np
+
+from DataStructeres import Stack, PriorityQueue
 
 
 def distance(x, y):
@@ -11,23 +12,32 @@ class Map:
     def __init__(self, m: np.ndarray) -> None:
         self.m = m
 
-    def neighbors(self, cell):
-        nrow, ncol = m.shape
+    @staticmethod
+    def neighbors(cell):
+        now, col = m.shape
         x, y = cell
         nb = []
         if x > 0:
             if m[x - 1, y] == 0:
                 nb = nb + [(x - 1, y)]
-        if x < (nrow - 1):
+        if x < (now - 1):
             if m[x + 1, y] == 0:
                 nb = nb + [(x + 1, y)]
         if y > 0:
             if m[x, y - 1] == 0:
                 nb = nb + [(x, y - 1)]
-        if y < (ncol - 1):
+        if y < (col - 1):
             if m[x, y + 1] == 0:
                 nb = nb + [(x, y + 1)]
         return nb
+
+    @staticmethod
+    def cost(node1, node2):
+        return 1
+
+    @staticmethod
+    def heuristic(node, goal_node):
+        return distance(node, goal_node)
 
 
 m = np.array(
@@ -38,73 +48,118 @@ m = np.array(
      [0, 0, 0, 1, 1, 0, 0],
      [0, 0, 0, 1, 1, 0, 0]])
 mm = Map(m)
-mm.neighbors((4, 1))
 
 
-class PriorityQueue:
-    def __init__(self):
-        self.elements = []
+# mm.neighbors((4, 1))
 
-    def empty(self):
-        return len(self.elements) == 0
-
-    def put(self, item, priority):
-        heapq.heappush(self.elements, (priority, item))
-
-    def get(self):
-        return heapq.heappop(self.elements)[1]
+# for l in m:
+#     print(l)
+# print()
 
 
-class Stack:
-    def __init__(self):
-        self.elements = []
-
-    def empty(self):
-        return len(self.elements) == 0
-
-    def put(self, x):
-        self.elements.append(x)
-
-    def get(self):
-        return self.elements.pop()
-
-
-def make_path(came_from, start, goal):
-    current = goal
+def make_path(came_from, start_node, goal_node):
+    current = goal_node
     path = []
-    while current != start:
+    while current != start_node:
         path.append(current)
         current = came_from[current]
-    path.append(start)
+    path.append(start_node)
     path.reverse()
     return path
 
 
-def dfs(map, start, goal):
+def dfs(labyrinth, start_node, goal_node):
     stack = Stack()
-    stack.put(start)
+    stack.put(start_node)
     visited = set()
-    came_from = {start: None}
+    came_from = {start_node: None}
 
     while not stack.empty():
         current = stack.get()
-        if current == goal:
-            return make_path(came_from, start, goal)
+        if current == goal_node:
+            return make_path(came_from, start_node, goal_node)
 
-        for next in map.neighbors(current):
-            if next not in visited:
-                stack.put(next)
-                visited.add(next)
-                came_from[next] = current
+        for next_node in labyrinth.neighbors(current):
+            if next_node not in visited:
+                stack.put(next_node)
+                visited.add(next_node)
+                came_from[next_node] = current
     return None
 
 
-def astar(map, start, goal):
-    pass
+def astar(labyrinth, start_node, goal_node):
+    # Create a priority queue
+    queue = PriorityQueue()
+    queue.put(start_node, labyrinth.heuristic(start_node, goal_node))
+    # Used for reconstructing the path at the end
+    came_from = {start_node: None}
+    # Cost to travel to each node
+    cost = {start_node: 0}
+
+    # Loop until there are no nodes left to explore
+    while not queue.empty():
+        # Get the current node with the lowest cost
+        current = queue.get()
+
+        # If the goal has been reached, reconstruct and return the path
+        if current == goal_node:
+            return make_path(came_from, start_node, goal_node)
+
+        # Explore the neighbors of the current node
+        for neighbor in labyrinth.neighbors(current):
+            # Calculate the new cost to the neighbor
+            new_cost = cost[current] + labyrinth.cost(current, neighbor)
+
+            # If the neighbor hasn't been visited or a cheaper path has been found
+            if neighbor not in cost or new_cost < cost[neighbor]:
+                # Update the cost and path to the neighbor
+                cost[neighbor] = new_cost
+                priority = new_cost + labyrinth.heuristic(neighbor, goal_node)
+                queue.put(neighbor, priority)
+                came_from[neighbor] = current
+
+    # If the goal was never reached
+    return None
 
 
-# Testing dfs function from (0,0) to (5,6)
-path_dfs = dfs(mm, (0, 0), (5, 6))
+# Run DFS and A_Star to find the path
+start = (0, 0)
+goal = (5, 6)
+path_dfs = dfs(mm, start, goal)
+path_astar = astar(mm, start, goal)
 
-print(path_dfs)
+
+# Visualize the map and path
+def visualize_map_and_path(map_data, path):
+    # Create a new figure and axis for the plot
+    fig, ax = plt.subplots(figsize=(6, 5))
+
+    # Set up the colormap: obstacles are yellow, path and free space are purple
+    norm = plt.Normalize(vmin=map_data.min(), vmax=map_data.max())
+
+    # Create a color mapping: 1 is mapped to yellow, 0 is purple
+    colors = [[norm(0), "purple"], [norm(1), "yellow"]]
+    cmap = plt.cm.colors.LinearSegmentedColormap.from_list("", colors)
+
+    # Plot the map
+    ax.imshow(map_data, cmap=cmap, norm=norm)
+
+    # Plot the path
+    if path:
+        # Convert a path to a numpy array for convenience
+        path = np.array(path)
+        ax.plot(path[:, 1], path[:, 0], color='black', linewidth=2)
+
+    # Turn off the axis labels
+    ax.axis('off')
+
+    # Show the plot
+    plt.show()
+
+
+# Call the visualization function
+visualize_map_and_path(m, path_dfs)
+visualize_map_and_path(m, path_astar)
+
+# print(path_dfs)
 # print(path_astar)
